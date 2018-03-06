7b7ecc16916440a60c8a6ca9631227dd5fc09851
chenggang321 <1129137164>
2018-03-06 17:38:04 +0800

    完善购物车功能

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import goodsList from '@/views/goodsList/goodsList'
 import cart from '@/views/cart/cart'
+import address from '@/views/address/address'
+import orderConfirm from '@/views/orderConfirm/orderConfirm'
+import orderSuccess from '@/views/orderSuccess/orderSuccess'
 
 Vue.use(Router)
 
@@ -17,8 +20,23 @@ export default new Router({
     },
     {
       path: '/cart',
-      name: 'Cart',
+      name: 'cart',
       component: cart
+    },
+    {
+      path: '/address',
+      name: 'address',
+      component: address
+    },
+    {
+      path: '/orderConfirm',
+      name: 'orderConfirm',
+      component: orderConfirm
+    },
+    {
+      path: '/orderSuccess',
+      name: 'orderSuccess',
+      component: orderSuccess
     }
   ]
 })
--- src/views/cart/cart.vue
+++ src/views/cart/cart.vue
@@ -126,7 +126,7 @@
                 总价: <span class="total-price">{{totalPrice | currency('￥')}}</span>
               </div>
               <div class="btn-wrap">
-                <a class="btn btn--red">去结算</a>
+                <a class="btn btn--red" v-bind:class="{'btn--dis':checkedCount===0}" @click="checkOut">去结算</a>
               </div>
             </div>
           </div>
@@ -255,6 +255,13 @@
             console.log('update suc');
           }
         });
+      },
+      checkOut () {
+        if (this.checkedCount > 0) {
+          this.$router.push({
+            path: '/address'
+          });
+        }
       }
     }
   }
